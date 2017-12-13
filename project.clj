3bec48d1ad1bb6c357e716b8e7f09e1426931ac2
Chad Harrington <chad@deercreeklabs.com>
2017-12-13 12:52:56 -0700

    Upgrade deps

--- project.clj
+++ project.clj
@@ -88,7 +88,7 @@
     :exclusions [org.apache.commons/commons-compress
                  org.clojure/clojure org.slf4j/slf4j-api]]
    [deercreeklabs/log-utils "0.1.2"]
-   [deercreeklabs/tube "0.1.17" :exclusions [org.slf4j/slf4j-api]]
+   [deercreeklabs/tube "0.1.18" :exclusions [org.slf4j/slf4j-api]]
    [org.clojure/clojure "1.9.0"]
    [org.clojure/clojurescript "1.9.946"]
    [org.clojure/core.async "0.3.465"]
