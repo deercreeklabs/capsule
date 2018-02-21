9f519a2f6170dc827edac15ec7dd7695fccad5ea
Chad Harrington <chad@deercreeklabs.com>
2018-02-20 19:50:32 -0700

    lein-release plugin: bumped version from 0.2.23 to 0.2.24-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -39,7 +39,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/capsule "0.2.23"
+(defproject deercreeklabs/capsule "0.2.24-SNAPSHOT"
   :description "A semantic layer for tube"
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
