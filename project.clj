9d0afe3537b0d62ff3e3b42b0843a2eab55b7a1e
Chad Harrington <chad@deercreeklabs.com>
2018-05-01 19:10:30 -0600

    Upgrade deps.

--- project.clj
+++ project.clj
@@ -85,12 +85,12 @@
    [com.fzakaria/slf4j-timbre "0.3.8"]
    [com.taoensso/timbre "4.10.0"]
    [deercreeklabs/async-utils "0.1.9"]
-   [deercreeklabs/baracus "0.1.4"]
-   [deercreeklabs/lancaster "0.1.20"
+   [deercreeklabs/baracus "0.1.5"]
+   [deercreeklabs/lancaster "0.1.21"
     :exclusions [org.apache.commons/commons-compress
                  org.clojure/clojure org.slf4j/slf4j-api]]
-   [deercreeklabs/log-utils "0.1.4"]
-   [deercreeklabs/tube "0.1.27" :exclusions [org.slf4j/slf4j-api]]
+   [deercreeklabs/log-utils "0.1.5"]
+   [deercreeklabs/tube "0.1.28" :exclusions [org.slf4j/slf4j-api]]
    [org.clojure/clojure "1.9.0"]
    [org.clojure/clojurescript "1.10.238"]
    [org.clojure/core.async "0.4.474"]
