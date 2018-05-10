5b1fa5310e9fdf32c15cbb7f5380413d247a948d
Chad Harrington <chad@deercreeklabs.com>
2018-05-10 17:47:18 -0600

    lein-release plugin: preparing 0.3.5 release

--- project.clj
+++ project.clj
@@ -40,7 +40,7 @@
              :compiler compiler}
       node-test? (assoc :notify-command ["node" output-to]))))
 
-(defproject deercreeklabs/capsule "0.3.5-SNAPSHOT"
+(defproject deercreeklabs/capsule "0.3.5"
   :description "A semantic layer for tube"
   :url "http://www.deercreeklabs.com"
   :license {:name "Apache License, Version 2.0"
