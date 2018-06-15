73438e702878bb04225de7ac88281b1f06c277dc
Chad Harrington <chad@deercreeklabs.com>
2018-06-15 17:39:43 -0600

    Upgrade deps

--- project.clj
+++ project.clj
@@ -84,15 +84,15 @@
    [binaryage/oops "0.6.2"]
    [com.fzakaria/slf4j-timbre "0.3.8"]
    [com.taoensso/timbre "4.10.0"]
-   [deercreeklabs/async-utils "0.1.9"]
-   [deercreeklabs/baracus "0.1.6"]
-   [deercreeklabs/lancaster "0.1.37"
+   [deercreeklabs/async-utils "0.1.10"]
+   [deercreeklabs/baracus "0.1.8"]
+   [deercreeklabs/lancaster "0.1.38"
     :exclusions [org.apache.commons/commons-compress
                  org.clojure/clojure org.slf4j/slf4j-api]]
-   [deercreeklabs/log-utils "0.1.6"]
-   [deercreeklabs/tube "0.1.29" :exclusions [org.slf4j/slf4j-api]]
+   [deercreeklabs/log-utils "0.1.7"]
+   [deercreeklabs/tube "0.1.30" :exclusions [org.slf4j/slf4j-api]]
    [org.clojure/clojure "1.9.0"]
-   [org.clojure/clojurescript "1.10.238"]
+   [org.clojure/clojurescript "1.10.312"]
    [org.clojure/core.async "0.4.474"]
    [primitive-math "0.1.6"]
    [prismatic/schema "1.1.9"]]
